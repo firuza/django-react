2386fbbafad2e656c4d69cbdbb706579c79d154c
firuza <firya87@gmail.com>
2020-09-01 23:14:34 +0530

    API for ProjectInfo

--- myproject/backend/views.py
+++ myproject/backend/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import ProjectInfo
+from rest_framework import viewsets
+from .serializers import ProjectInfoSerializer
 
 # Create your views here.
+
+class ProjectInfoViewSet(viewsets.ModelViewSet):
+    queryset = ProjectInfo.objects.filter(show=True)
+    serializer_class = ProjectInfoSerializer
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+from backend.views import ProjectInfoViewSet
+
+router = routers.DefaultRouter()
+router.register('api/projects',ProjectInfoViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
