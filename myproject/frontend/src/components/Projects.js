e57dccd28d07d84dde8f01d418c588a6e94dc3b2
firuza <firya87@gmail.com>
2020-09-03 23:42:41 +0530

    fetch project using API

--- myproject/frontend/src/components/Projects.js
+++ myproject/frontend/src/components/Projects.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import ProjectCard from './ProjectCard';
-import { Container, Row, Col, CardColumns } from 'react-bootstrap';
-
+import projects from '../apis/projects'
+import { Container, CardColumns } from 'react-bootstrap';
 
 const Projects = () => {
+
+    const [projectInfo,setProjectInfo] = useState([]);
+
+    const fetchProjects  = async () =>{
+        const response = await projects.get('/projects/')
+        setProjectInfo(response.data)
+    }
+
+    useEffect(()=>{
+        fetchProjects()
+    },[])
+
     return (
         <>
             <h2>Project Component</h2>
+            <h2>Total: {projectInfo.length}</h2>
+            {console.log(projectInfo)}
+
             <Container>
                 <CardColumns>
                     <ProjectCard />
