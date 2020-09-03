22cfb6ad473e6bf96ce4641b3a170034410d50c6
firuza <firya87@gmail.com>
2020-09-03 19:25:11 +0530

    add cols cards

--- myproject/frontend/src/components/Projects.js
+++ myproject/frontend/src/components/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, CardColumns } from 'react-bootstrap';
 
 
 const Projects = () => {
@@ -8,16 +8,14 @@ const Projects = () => {
         <>
             <h2>Project Component</h2>
             <Container>
-                <Row>
-                    <Col><ProjectCard /></Col>
-                    <Col><ProjectCard /></Col>
-                    <Col><ProjectCard /></Col>
-                </Row>
-                <Row>
-                    <Col><ProjectCard /></Col>
-                    <Col><ProjectCard /></Col>
-                    <Col><ProjectCard /></Col>
-                </Row>
+                <CardColumns>
+                    <ProjectCard />
+                    <ProjectCard />
+                    <ProjectCard />
+                    <ProjectCard />
+                    <ProjectCard />
+                    <ProjectCard />
+                </CardColumns>
             </Container>
         </>
     )
