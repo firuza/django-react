08b32f665c6c2cb986c320256b660ce3dbef83c8
firuza <firya87@gmail.com>
2020-09-03 18:48:55 +0530

    UI change

--- myproject/frontend/src/components/Skills.js
+++ myproject/frontend/src/components/Skills.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SkillsCards from './SkillsCards'
 
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 
 const Skills = () => {
 
@@ -23,17 +23,19 @@ const Skills = () => {
         <>
             <h1>Skills</h1>
 
-            <Row>
-                <Col xs={4}>
-                    <SkillsCards skills = {frontend} />
-                </Col>
-                <Col xs={4}>
-                    <SkillsCards skills = {backend} />
-                </Col>
-                <Col xs={4}>
-                    <SkillsCards skills = {gameDev} />
-                </Col>                
-            </Row>
+            <Container>
+                <Row>
+                    <Col xs={4}>
+                        <SkillsCards skills = {frontend} />
+                    </Col>
+                    <Col xs={4}>
+                        <SkillsCards skills = {backend} />
+                    </Col>
+                    <Col xs={4}>
+                        <SkillsCards skills = {gameDev} />
+                    </Col>                
+                </Row>
+            </Container>
         </>
     )
 }
